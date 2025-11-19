b6014f34c95a7f863b0b5742f3afb038d94b0d06
raonrabbit <raonrabbit@gmail.com>
2025-11-19 10:52:20 +0900

    feat: Implement company carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
-import { CompetitionSchedule, HeroCarousel, OurRole } from "@/widgets/main";
+import {
+    Community,
+    CompanyCarousel,
+    CompetitionNews,
+    CompetitionSchedule,
+    HeroCarousel,
+    OurRole,
+} from "@/widgets/main";
 
 export default function Home() {
     return (
         <div className="bg-kua-white flex items-center justify-center">
-            <main className="flex w-full flex-col items-center justify-between gap-24 px-16 pb-24">
+            <main className="flex w-full flex-col items-center justify-between gap-24 px-16">
                 <HeroCarousel />
                 <OurRole />
                 <CompetitionSchedule />
+                <Community />
+                <CompetitionNews />
+                <CompanyCarousel />
             </main>
         </div>
     );
