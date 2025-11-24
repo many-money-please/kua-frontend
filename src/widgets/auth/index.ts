c2581e5c91148bdd84d818dc80f22f6b930afc6a
이재백 <dbswlrla112@naver.com>
2025-11-24 22:54:05 +0900

    feat: Implements change password page components

--- src/widgets/auth/index.ts
+++ src/widgets/auth/index.ts
@@ -1,3 +1,4 @@
 export { AuthTabs } from "./ui/AuthTabs";
 export { LoginForm } from "./ui/LoginForm";
 export { RegisterForm } from "./ui/RegisterForm";
+export { ChangePasswordForm } from "./ui/ChangePasswordForm";
