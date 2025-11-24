29e922709d0d39f36472646f03cd19430373a412
raonrabbit <raonrabbit@gmail.com>
2025-11-25 03:36:39 +0900

    feat: Change and implement details

--- src/widgets/fin-swimming/ui/FinSwimmingTabs.tsx
+++ src/widgets/fin-swimming/ui/FinSwimmingTabs.tsx
@@ -5,14 +5,11 @@ import { BaseTabs } from "@/shared/ui/Tabs";
 const tabs = [
     { label: "유래", path: "/fin-swimming/history" },
     { label: "기술 및 훈련", path: "/fin-swimming/skills-and-training" },
+    { label: "민간자격등록", path: "/fin-swimming/private-qualification" },
 ];
 
 export const FinSwimmingTabs = () => {
     return (
-        <BaseTabs
-            tabs={tabs}
-            className="mx-auto w-full max-w-[1200px] py-20"
-        />
+        <BaseTabs tabs={tabs} className="mx-auto w-full max-w-[1200px] py-20" />
     );
 };
-
