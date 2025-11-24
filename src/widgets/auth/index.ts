4cfcd27f2e5e4eace2c95dcd78f5022a7b345ba2
이재백 <dbswlrla112@naver.com>
2025-11-24 22:54:35 +0900

    feat: Implements find password page components

--- src/widgets/auth/index.ts
+++ src/widgets/auth/index.ts
@@ -2,4 +2,5 @@ export { AuthTabs } from "./ui/AuthTabs";
 export { LoginForm } from "./ui/LoginForm";
 export { RegisterForm } from "./ui/RegisterForm";
 export { FindIdForm } from "./ui/FindIdForm";
+export { FindPasswordForm } from "./ui/FindPasswordForm";
 export { ChangePasswordForm } from "./ui/ChangePasswordForm";
