ba5e7d7e420b1154d69a973f7287bd1361f8d443
이재백 <dbswlrla112@naver.com>
2025-11-24 22:54:24 +0900

    feat: Implements find id page components

--- src/widgets/auth/index.ts
+++ src/widgets/auth/index.ts
@@ -1,4 +1,5 @@
 export { AuthTabs } from "./ui/AuthTabs";
 export { LoginForm } from "./ui/LoginForm";
 export { RegisterForm } from "./ui/RegisterForm";
+export { FindIdForm } from "./ui/FindIdForm";
 export { ChangePasswordForm } from "./ui/ChangePasswordForm";
